3319ea7ae1b994f5c3247087deca75ef1ee16cb4
Jerry <jerrywei998@gmail.com>
2022-01-10 00:46:55 -0500

    Allow user save and select saved note.

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,30 +1,19 @@
 const router = require('express').Router();
-const { } = require('../../lib/notesHelper');
-const { notes } = require('../../db/db');
+const { validateNote, createNewNote } = require('../../lib/notesHelper');
+const { notes } = require("../../db/db.json");
+const { v1: uuidv1 } = require('uuid');
 
 router.get('/notes', (req, res) => {
   let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
   res.json(results);
-});
-
-router.get('/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
 });
 
 router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
+  req.body.id = uuidv1()
 
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
+  if (!validateNote(req.body)) {
+    res.status(400).send('Missing data.');
   } else {
     const note = createNewNote(req.body, notes);
     res.json(note);
