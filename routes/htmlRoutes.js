779586f6664aefbb88798231961644586bf9ec5c
HankHint <hankhint@gmail.com>
2022-02-25 22:15:01 -0500

    changed some formatting

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,13 +4,15 @@ const router = require('express').Router();
 
 // GET /notes needs to return the notes.html file.
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './../public/notes.html'));
+    res.sendFile(path
+        .join(__dirname, './../public/notes.html'));
 });
 
 
 // GET * needs to return the index.html file.
 router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './../public/index.html'));
+    res.sendFile(path
+        .join(__dirname, './../public/index.html'));
 });
 
 module.exports = router;
