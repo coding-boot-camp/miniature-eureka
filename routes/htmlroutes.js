a15bef18060ca076031b9b7368a2fe11923198fb
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-07-06 17:50:07 -0700

    added html routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,12 @@
+const htmlPath = require('express').Router();
+const path = require('path');
+
+htmlPath.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+htmlPath.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+
+module.exports = htmlPath;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const api = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,9 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 app.use(express.static('public'));
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} :rocket:`)
