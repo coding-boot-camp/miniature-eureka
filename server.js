7f5d3efa671eca320fd3df0b70c110f8f8063a8f
sumtwelve <sumtwelve@gmail.com>
2023-03-24 20:25:17 -0600

    remove app.listen() from server.js

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.get('/notes', (req, res) =>
 );
 
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+// app.listen(PORT, () =>
+//   console.log(`App listening at http://localhost:${PORT} 🚀`)
+// );
