f51e32cb018bba91e9c04c0a9022eb464a599ddf
Jeffrey Yeh <jeffreyyehuw@gmail.com>
2023-04-01 14:13:56 -0700

    notes page appears

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// HTML route for index.html
+
+// HTML route for notes.html
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+// Listen for connections
+app.listen(PORT, () =>
+  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+);
