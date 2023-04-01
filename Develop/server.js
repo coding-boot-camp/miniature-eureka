1ba75989498c30673630f6e42adc4d87e89b660d
Jeffrey Yeh <jeffreyyehuw@gmail.com>
2023-04-01 14:17:12 -0700

    initialized landing page

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,9 @@ const PORT = process.env.port || 3001;
 const app = express();
 
 // HTML route for index.html
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+)
 
 // HTML route for notes.html
 app.get('/notes', (req, res) => 
