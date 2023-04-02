e8cd56cdef18ad38e5d7e185bbf2e14a3dc48772
Jeffrey Yeh <jeffreyyehuw@gmail.com>
2023-04-02 12:42:24 -0700

    implemented static assets

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const path = require('path');
+const notesData = require('./db/db.json');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
-// HTML route for index.html
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-)
+// Render CSS for notes page with static assets
+app.use(express.static('public'));
 
 // HTML route for notes.html
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+// API route: get request to read db.json and return all saved notes as JSON
+app.get('/api/notes', (req, res) => res.json(notesData))
+
+// API route: post request to add new notes to db.json + return new note to client
+
+// Wildcard HTML route for index.html
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 )
 
 // Listen for connections
