189dc34afe1e8f0fc7cc379ffcb4392006b6d1f1
Riley O'Neil <roneilwork@gmail.com>
2023-05-15 16:25:06 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Note Taker Starter Code
+This is a simple web application for adding Notes.
+
+## Purpose
+This was a challenge to build a note taking web app with a simple database structure supported by a json file.
+
+## Resources
+This code was forked from https://github.com/coding-boot-camp/miniature-eureka
+
+## Live Site
+check out the live github pages @ 
+
+## Usage
+[MIT](https://choosealicense.com/licenses/mit/)
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,20 +1,59 @@
-const path = require('path');
 const router = require('express').Router();
 const fs = require('fs');
 const util = require('util');
 const noteData = require('../../db/db.json');
-// const {somethingName, postNotes} = require("../../controller/noteController.js");
+const uuid = require('uuid');
 
 const readFromFile = util.promisify(fs.readFile);
 
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            const parsedData = JSON.parse(data)
+            parsedData.push(content);
+            writetoFile(file, parsedData);
+        }
+    })
+}
+
+const writetoFile = (file, content) => {
+    fs.writeFile(file, JSON.stringify(content, null, 4), (err) => 
+        err ? console.log(err) : console.log(`Data written to ${file}!`)
+    )
+}
+
 
 router.get('/', function(req, res) {
-    res.send(noteData);
+    readFromFile('./db/db.json').then((data) => res.send(data));
 });
-// router.post(postNotes)
 
-// router.get('/', function (req, res) {
-//     res.send(JSON.parse("Heres a thing"));
+router.delete('/:id', function(req, res) {
+    const {id} = req.params.id;
+});
 
-// })
+router.post('/', function(req, res) {
+    const {title, text} = req.body;
+
+    if(text && title) {
+        const newNote = {
+            title,
+            text,
+            id: uuid.v4()
+        }
+
+        readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+    } else {
+        res.json('Error in posting feedback');
+    }
+})
+
 module.exports = router;
