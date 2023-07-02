9b3b4d6cf5474c2727b7b243762f583fb5752071
Koro999 <chris.dang93@hotmail.com>
2023-07-02 11:58:08 -0400

    delete route is working as intended, but it's not actively updating the front end

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -8,7 +8,8 @@ const {
   readAndAppend,
   writeToFile,
 } = require('../helpers/fsUtils');
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
+const { readFile } = require('fs');
 
 //GET route that retrieves all the notes 
 notes.get('/', (req,res) =>
@@ -40,9 +41,33 @@ notes.post('/', (req,res) => {
         res.json('Error in saving notes ')
     }
 });
-/*
-app.delete('/api/notes/:id', (req,res) => 
-  
-);*/
+
+//DELETE notes based on ids
+//app.delete(path, callback, [callback])
+//'/api/notes/:id'
+notes.delete('/:id', async (req,res) => {
+    //req.params, contains route parameters
+    //IF the parameters are specified when a URL is built, then req.parms object will be populated with said URL
+    //in this example it's :id 
+    //you can chain this if you wanted to pull other parameters ex. '/:id/:text'
+    let { id } = req.params;
+    //returns the id of the note clicked
+    console.log(id)
+    //parse the JSON file and save it as an object for comparison
+    var db = JSON.parse(fs.readFileSync('./db/db.json'))
+    console.log(db)
+    //for..in, the obj 
+    //note represents the index basically
+    for(const note in db) {
+        //IF id matches the id of the object at the specific index
+        if(id == db[note].id) {
+            //remove the object in the array at that index
+            db.splice([note], 1)
+        }
+    }
+    //write the information back to the original JSON to update it 
+    //stringify parameters, are replacer and spacing 
+    fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 4))
+});
 
 module.exports = notes;
