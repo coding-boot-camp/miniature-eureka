399c14472a08e1f38bb7f2bf913beea985af9853
ytbarek <130870489+3aljeet@users.noreply.github.com>
2023-11-20 12:38:40 -0800

    Update apiRoutes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,63 +1,33 @@
-// const fs = require('fs');
-// const path = require('path');
-// const dbPath = path.join(__dirname, '../db/db.json');
 
-// module.exports = (app) => {
-//   app.get('/api/notes', (req, res) => {
-//     // Read notes from db.json and return them as JSON.
-//     try {
-//       const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
-//       res.json(data);
-//     } catch (error) {
-//       res.status(500).json({ error: 'Error reading notes.' });
-//     }
-//   });
-
-//   app.post('/api/notes', (req, res) => {
-//     // Receive a new note, add it to db.json, and return the new note.
-//     const newNote = req.body;
-//     newNote.id = uuidv4(); // Generate a unique ID for the new note
-
-//     try {
-//       const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
-//       data.push(newNote);
-//       fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf8');
-//       res.json(newNote);
-//     } catch (error) {
-//       res.status(500).json({ error: 'Error saving the note.' });
-//     }
-//   });
-// };
-const router = require('express').Router;
+const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const { v4: uuid } = require('uuid');
+const { createNewNote } = require('../model/createNote'); // Assuming the correct path to the external module
 const dbPath = path.join(__dirname, '../db/db.json');
-const uuid  = require('uuid');
+const db = require(dbPath); // Import the db.json directly
 
+// Route to get notes from the database
+router.get('/api/notes', (req, res) => {
+  try {
+    res.json(db.notes); // Assuming that the notes are stored in the "notes" property of db.json
+  } catch (error) {
+    res.status(500).json({ error: 'Error reading notes.' });
+  }
+});
 
-  // Route to get notes from the database
-  router.get('/api/notes', (req, res) => {
-    try {
-      const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
-      res.json(data);
-    } catch (error) {
-      res.status(500).json({ error: 'Error reading notes.' });
-    }
-  });
+// Route to save a new note to the database
+router.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid(); // Generate a unique ID for the new note
 
-  // Route to save a new note to the database
-  router.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuid(); // Generate a unique ID for the new note
-
-    try {
-      const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
-      data.push(newNote);
-      fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf8');
-      res.json(newNote);
-    } catch (error) {
-      res.status(500).json({ error: 'Error saving the note.' });
-    }
-  });
+  try {
+    createNewNote(newNote, db.notes); // Assuming createNewNote is a function that adds the new note to the "notes" property of db.json
+    fs.writeFileSync(dbPath, JSON.stringify(db, null, 2), 'utf8');
+    res.json(newNote);
+  } catch (error) {
+    res.status(500).json({ error: 'Error saving the note.' });
+  }
+});
 
 module.exports = router;
