1d258245c82cd1d456bbfa34da5b3763f88587ad
ytbarek <130870489+3aljeet@users.noreply.github.com>
2023-11-20 12:34:54 -0800

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const path = require('path');
-// const uuid  = require('uuid');
+const uuid = require('./helpers/uuid'); // Corrected import
+
+const PORT = process.env.PORT || 3306;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // Import and use your route files (apiRoutes.js and htmlRoutes.js).
-app.use('/apiRoutes', apiRoutes);
-app.use('/htmlRoutes', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3306'); // Replace * with the actual origin of your client application
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-
-app.get('/notes', (req, res)=>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}`);
+  console.log(`App listening at http://localhost:` + PORT);
 });
- 
